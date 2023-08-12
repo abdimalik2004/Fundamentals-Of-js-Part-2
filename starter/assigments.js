9cd12c28839656c22b394aa0f3e7d7bec77a10d6
abdimalik abdirahman <mahadalleabdimalik@gmail.com>
2023-08-12 17:44:00 +0300

    updated

--- starter/assigments.js
+++ starter/assigments.js
@@ -1,3 +1,4 @@
+/*
 // LECTURE 33 functions
 function describeCountry(country, population, capitalCity) {
     const somaliia = `${country} has ${population} million  people and its
@@ -10,3 +11,21 @@ const kenya = describeCountry('kenya', 30, 'nairobi')
 console.log(somalia);
 console.log(ethipia);
 console.log(kenya);
+*/
+// LECTURE 34 : function declaration and expression
+function percentageOfWorld1(population) {
+    return (population / 7900) * 100;
+}
+
+const percentageOfWorld2 = function(population) {
+    return (population / 7900) * 100;
+}
+const persom = percentageOfWorld1(20);
+const pereth = percentageOfWorld1(145);
+const perkenya = percentageOfWorld1(30);
+console.log(persom, pereth, perkenya);
+
+const perSom = percentageOfWorld2(20);
+const perEth = percentageOfWorld2(145);
+const perKenya = percentageOfWorld2(30);
+console.log(perSom, perEth, perKenya);
