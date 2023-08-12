e84c18fc263f8446523983df8d0b1b72ddbf706e
abdimalik abdirahman <mahadalleabdimalik@gmail.com>
2023-08-12 15:22:49 +0300

    updated

---
+++ starter/script.js
@@ -0,0 +1,12 @@
+/*
+// LECTURE 32 Strict mode 
+'use strict';
+let hasDriverLicense = false;
+const passTest = true;
+
+if(passTest) hasDriverLicense = true;
+if(hasDriverLicense) console.log('I can drive');
+// const interface = 'Auto';
+// const private = 23;
+// these some of reserved words that in future will add in js
+*/
