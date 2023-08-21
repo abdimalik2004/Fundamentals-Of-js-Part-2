0d51ecc7f834e4738d07deb4254b9b389a589564
abdimalik abdirahman <mahadalleabdimalik@gmail.com>
2023-08-21 13:45:02 +0300

    updated

--- starter/challenges.js
+++ starter/challenges.js
@@ -1,3 +1,5 @@
+
+// CHALLENGE #1 
 const calcaverage = (a, b, c) => (a + b + c) / 3;
 console.log(calcaverage(3, 4, 5));
 // Test 1
@@ -19,5 +21,14 @@ checkWinner(scoreDolphins, scoreKolas);
 // Test 2
 scoreDolphins = calcaverage(85, 54, 41);
 scoreKolas = calcaverage(23, 34, 27);
-console.log(scoreDolphins, scoreKolas)
-checkWinner(scoreDolphins, scoreKolas)
+console.log(scoreDolphins, scoreKolas);
+checkWinner(scoreDolphins, scoreKolas);
+// CHALLENEGE #2
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+ 
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+ 
+console.log(bills, tips);
