d07793bf63e7ac7134b1ba8978c9eefc490dc08d
abdimalik abdirahman <mahadalleabdimalik@gmail.com>
2023-08-24 21:41:57 +0300

    updated

--- starter/challenges.js
+++ starter/challenges.js
@@ -32,3 +32,32 @@ const bills = [125, 555, 44];
 const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
  
 console.log(bills, tips);
+
+const mark = {
+    fullName: 'Mark Miller',
+    mass: 78,
+    height: 1.69,
+    calcBMI: function () {
+      this.bmi = this.mass / (this.height * this.height);
+      return this.bmi;
+    }
+  };
+   
+  const john = {
+    fullName: 'John Smith',
+    mass: 92,
+    height: 1.95,
+    calcBMI: function () {
+      this.bmi = this.mass / (this.height * this.height);
+      return this.bmi;
+    }
+  };
+   
+  mark.calcBMI();
+  john.calcBMI();
+   
+  if (mark.bmi > john.bmi) {
+    console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s (${john.bmi})!`)
+  } else if (john.bmi > mark.bmi) {
+    console.log(`${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s (${mark.bmi})!`)
+  }
