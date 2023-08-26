8e64f3ce40fa32b7d6faf5e3a38636811723800c
abdimalik abdirahman <mahadalleabdimalik@gmail.com>
2023-08-26 14:04:24 +0300

    updated

--- starter/challenges.js
+++ starter/challenges.js
@@ -1,4 +1,4 @@
-
+/* 
 // CHALLENGE #1 
 const calcaverage = (a, b, c) => (a + b + c) / 3;
 console.log(calcaverage(3, 4, 5));
@@ -32,7 +32,7 @@ const bills = [125, 555, 44];
 const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
  
 console.log(bills, tips);
-
+// CHALLENGE #3
 const mark = {
     fullName: 'Mark Miller',
     mass: 78,
@@ -61,3 +61,30 @@ const mark = {
   } else if (john.bmi > mark.bmi) {
     console.log(`${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s (${mark.bmi})!`)
   }
+*/
+  // CHALLENGE #4
+  function calcTip(bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+ 
+  /* Write your code below. Good luck! 🙂 */
+  const bills = [ 22, 295, 176, 440, 37, 105, 10, 1100, 86,52];
+  const tips = [];
+  const totals = [];
+  for(let i = 0; i < bills.length; i++) {
+      const tip = calcTip(bills[i]);
+      tips.push(tip);
+      totals.push(tip + bills[i]);
+      
+  }
+  console.log(bills, tips, totals);
+  const calcAverage = function(arr) {
+      let sum = 0;
+      for(let i = 0; i < arr.length; i++) {
+          sum += arr[i];
+      }
+      return sum / arr.length;
+  };
+  console.log(calcAverage([2, 3, 7]));
+  console.log(calcAverage(totals));
+  console.log(calcAverage(tips));
